3a9bf3f4a6fb03d84819a5bff9b81fc59c08c1b6
Joseph Murray <josephwmurray@gmail.com>
2023-10-18 17:07:07 -0700

    linked to new clients from main menu, more styling

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
+using System.Collections.Generic;
 
 namespace HairSalon.Controllers
 {
@@ -35,6 +36,26 @@ namespace HairSalon.Controllers
       };
       ThoseWhoStyle.AddClient(stylistId, newClient);
       return RedirectToAction("Clients", "Stylist", new { id = stylistId });
+    }
+
+    [HttpPost("Stylist/NewClient")]
+    public ActionResult AddClient(string clientName, int stylistId)
+    {
+      Client newClient = new Client
+      {
+        ClientName = clientName
+      };
+      ThoseWhoStyle.AddClient(stylistId, newClient);
+      return RedirectToAction("Menu", "Home");
+    }
+
+
+    [HttpGet("/clients/new")]
+    public ActionResult NewClient()
+    {
+      List<Stylist> stylists = ThoseWhoStyle.GetStylists();
+      ViewBag.Stylists = stylists;
+      return View("~/Views/Client/New.cshtml");
     }
 
   }
